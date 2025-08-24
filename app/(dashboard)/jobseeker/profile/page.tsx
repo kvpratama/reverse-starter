ee34bbb7d1e57b1fbde67ef568d6ba6a9a0b27c9
kvpratama <kvpratama@gmail.com>
2025-08-24 16:25:12 +0900

    update job seeker profile page

--- app/(dashboard)/jobseeker/profile/page.tsx
+++ app/(dashboard)/jobseeker/profile/page.tsx
@@ -4,10 +4,12 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
+  CardFooter,
 } from "@/components/ui/card";
 import { getSession } from "@/lib/auth/session";
 import { getJobseekerProfiles } from "@/lib/db/queries";
-
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 export default async function JobseekerProfilePage() {
   const session = await getSession();
   const profiles = await getJobseekerProfiles(session?.user?.id || "");
@@ -21,23 +23,32 @@ export default async function JobseekerProfilePage() {
             <CardDescription>{profile.email}</CardDescription>
           </CardHeader>
           <CardContent>
-            <p className="text-sm text-gray-500">Bio: {profile.bio}</p>
-            <p className="text-sm text-gray-500">Skills: {profile.skills}</p>
-            <p className="text-sm text-gray-500">
+            <p className="text-sm text-gray-500">{profile.bio ? profile.bio.slice(0, 100) : ''}...</p>
+            {/* <p className="text-sm text-gray-500">Skills: {profile.skills}</p> */}
+            {/* <p className="text-sm text-gray-500">
               Experience: {profile.experience}
-            </p>
-            <p className="text-sm text-gray-500">
+            </p> */}
+            {/* <p className="text-sm text-gray-500">
               Desired Salary: ${profile.desiredSalary}
-            </p>
-            <a
+            </p> */}
+            {/* <a
               href={profile.resumeUrl}
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-500 hover:underline"
             >
               View Resume
-            </a>
+            </a> */}
+            
           </CardContent>
+          <CardFooter>
+          <Link href={`/jobseeker/profile/${profile.id}`}>
+              <Button variant="outline" className="bg-orange-500 hover:bg-orange-600 text-white hover:text-white rounded-full hover:cursor-pointer">
+                View Profile
+              </Button>
+            </Link>
+            </CardFooter>
+          
         </Card>
       ))}
     </div>
