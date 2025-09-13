352ee6fe4026b41c25a8fa501d4223232f59d669
kvpratama <kvpratama@gmail.com>
2025-09-13 16:01:57 +0900

    add nationality, visa_status, age to jobseeker_profile

---
+++ lib/db/migrations/0001_short_microchip.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "jobseekers_profile" ADD COLUMN "nationality" varchar(50);--> statement-breakpoint
+ALTER TABLE "jobseekers_profile" ADD COLUMN "visa_status" varchar(20);--> statement-breakpoint
+ALTER TABLE "jobseekers_profile" ADD COLUMN "age" integer;
