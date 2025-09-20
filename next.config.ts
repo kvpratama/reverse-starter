90c3559949bc499f3a76d00d82c26d2808e01cf5
kvpratama <kvpratama@gmail.com>
2025-09-20 18:35:31 +0900

    turn of experimental clientSegmentCache causing skeleton fail to load

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     // ppr: false,
-    clientSegmentCache: true,
+    // clientSegmentCache: true,
     // nodeMiddleware: true,
   },
 };
