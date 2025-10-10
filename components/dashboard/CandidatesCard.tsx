047527aedcf232eaaa798b5807ba2a02df60195e
kvpratama <kvpratama@gmail.com>
2025-10-10 19:28:19 +0900

    comment out console.log

--- components/dashboard/CandidatesCard.tsx
+++ components/dashboard/CandidatesCard.tsx
@@ -154,7 +154,7 @@ export default function CandidatesCard({
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {processedCandidates.map((c) => {
-                console.log(c);
+                // console.log(c);
                 return (
                   <CandidateCard
                     key={c.candidateId}
